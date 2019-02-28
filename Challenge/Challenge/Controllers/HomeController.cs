d5794e5c6a7b062ec5335c70fefa3f73e235a5ad
Yeison Bernal <yeison.bernal@endava.com>
2019-02-28 15:19:40 -0500

    Read Json Done

--- Challenge/Challenge/Controllers/HomeController.cs
+++ Challenge/Challenge/Controllers/HomeController.cs
@@ -5,14 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Challenge.Models;
+using System.Net;
+using Newtonsoft.Json;
+using ClassLibrary1;
 
 namespace Challenge.Controllers
 {
     public class HomeController : Controller
     {
+        List<JsonFormat.AsteroidInfo> informationList = new List<JsonFormat.AsteroidInfo>();
+
         public IActionResult Index()
         {
-            return View();
+            informationList = CallApi();
+            return View(informationList);
         }
 
         public IActionResult Privacy()
@@ -24,6 +30,27 @@ namespace Challenge.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+
+        public List<JsonFormat.AsteroidInfo> CallApi()
+        {
+            List<JsonFormat.AsteroidInfo> information = new List<JsonFormat.AsteroidInfo>();
+   
+            var webClient = new WebClient();
+            var json = webClient.DownloadString(@"https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=rNpnnsUuVTCAxCN7ILVbLhpPMJiWzqUutrHI85De");
+            JsonFormat.Asteroid asteroids = JsonConvert.DeserializeObject<JsonFormat.Asteroid>(json);
+
+            foreach (KeyValuePair<string, List<JsonFormat.AsteroidInfo>> date in asteroids.near_earth_objects)
+            {
+                foreach (JsonFormat.AsteroidInfo value in date.Value)
+                {
+                    information.Add(value);
+                }
+            }
+            return information;
         }
     }
 }
